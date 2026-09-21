7bffb5f0a45bf517e184c2240d99b1d09a40f07b
ramymd79 <319159419+ramymd79@users.noreply.github.com>
2026-09-21 23:49:49 +0300

    Publish Daftar demo to GitHub Pages.

---
+++ _next/static/FmuKH4mjnpxcqG6tUCTVe/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Daftar//_next/:path+",
+        "destination": "/Daftar/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
